1985ed5d666537b3ebb67ce09693b5bd69b8b47f
Uneeck <urbantomasz87@gmail.com>
2017-02-24 17:03:53 +0100

    initial

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+animal = animal.toUpperCase();
+var text3 = text.replace('Papugi', animal);
+console.log(text3.substr(0, text3.length/2));
+
